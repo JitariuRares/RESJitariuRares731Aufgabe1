bfa037241ee0af3b2902d5a0e1e465520a3a3451
asus <jitariu_stefan@yahoo.com>
2025-02-18 11:54:44 +0200

    a1.a

---
+++ GameProcessor.java
@@ -0,0 +1,16 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class GameProcessor {
+
+    public static List<Game> filterGamesByGlobalerEinfluss(List<Game> games, int min) {
+        return games.stream()
+                .filter(game -> game.getGlobalerEinfluss() >= min)
+                .collect(Collectors.toList());
+    }
+}
+
+
